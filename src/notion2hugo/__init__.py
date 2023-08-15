3f085937126fbb7656a7145440187bf555bc000e
Chintak Sheth <chintak@fb.com>
2023-08-15 16:43:55 -0700

    add bumpver versioning control

--- src/notion2hugo/__init__.py
+++ src/notion2hugo/__init__.py
@@ -1,5 +1,7 @@
 import os
 
+__version__ = "0.0.1"
+
 assert "NOTION_TOKEN" in os.environ, "NOTION_TOKEN env variable not found!"
 NOTION_TOKEN = os.environ["NOTION_TOKEN"]
 NOTION_DATABASE_ID = os.environ["NOTION_DATABASE_ID"]
